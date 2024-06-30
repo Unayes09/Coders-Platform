585fc8023b49ba968b57f70a3702e9d15af14b49
Mutah3r <evil.programmer.stephen@gmail.com>
2024-06-30 17:41:22 +0600

    setup react-router

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,15 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
 import "./index.css";
 import { NextUIProvider } from "@nextui-org/react";
+import { RouterProvider } from "react-router-dom";
+import router from "./routes/router.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <NextUIProvider>
-      <main className="dark text-foreground bg-background">
-        <App />
-      </main>
-    </NextUIProvider>
+    <main className="dark text-foreground bg-background">
+      <RouterProvider router={router}>
+        <NextUIProvider></NextUIProvider>
+      </RouterProvider>
+    </main>
   </React.StrictMode>
 );
