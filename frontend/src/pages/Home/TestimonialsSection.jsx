bfe83130c7dec73ac32597057302515e9ec43cc3
Mutah3r <evil.programmer.stephen@gmail.com>
2024-07-03 05:32:11 +0600

    create testimonial section on homepage

---
+++ frontend/src/pages/Home/TestimonialsSection.jsx
@@ -0,0 +1,11 @@
+const TestimonialsSection = () => {
+  return (
+    <div>
+      <h1 className="text-center py-5 bg-red-500">
+        This is testimonial section
+      </h1>
+    </div>
+  );
+};
+
+export default TestimonialsSection;
