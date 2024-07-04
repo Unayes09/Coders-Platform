6e93493f6e560a6c3efb6ded364569d3faa0841c
Mutah3r <evil.programmer.stephen@gmail.com>
2024-07-04 22:10:44 +0600

    add todo msg on sign up page

--- frontend/src/pages/SignUp/SignUpForm.jsx
+++ frontend/src/pages/SignUp/SignUpForm.jsx
@@ -3,6 +3,7 @@ import { Link } from "react-router-dom";
 import axios from "axios";
 
 const SignUpForm = () => {
+  // TODO: as individual or company
   const handleFormSubmit = (event) => {
     event.preventDefault();
 
