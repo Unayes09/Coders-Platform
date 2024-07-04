032e4251db0e6ad03f96f01f8059dc1840ae6141
Mutah3r <evil.programmer.stephen@gmail.com>
2024-07-04 15:38:13 +0600

    add ventajs globe effect on login page

--- frontend/src/pages/Login/Login.jsx
+++ frontend/src/pages/Login/Login.jsx
@@ -1,7 +1,32 @@
+import VentaGlobe from "../../components/VentaGlobe/VentaGlobe";
+
 const Login = () => {
   return (
-    <div>
-      <h1>This is login page</h1>
+    <div className="">
+      <div className="relative">
+        <div className="z-10">
+          <VentaGlobe />
+        </div>
+        <div
+          className="p-6 absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 flex justify-center items-center rounded-2xl overflow-hidden shadow-lg text-white bg-[#0000003b] bg-opacity-80 backdrop-filter backdrop-blur-lg"
+          style={{ boxShadow: "0 0 10px rgba(0, 0, 0, 0.5)" }}
+        >
+          <div className="login-form">
+            <h2>Login</h2>
+            <form>
+              <div>
+                <label htmlFor="username">Username</label>
+                <input type="text" id="username" name="username" />
+              </div>
+              <div>
+                <label htmlFor="password">Password</label>
+                <input type="password" id="password" name="password" />
+              </div>
+              <button type="submit">Login</button>
+            </form>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
