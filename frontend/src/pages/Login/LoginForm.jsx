7dc9038c84eefd50f2b248925a1538b8fc77c502
Mutah3r <evil.programmer.stephen@gmail.com>
2024-07-13 14:21:28 +0600

    save user profile on localhost after they log in

--- frontend/src/pages/Login/LoginForm.jsx
+++ frontend/src/pages/Login/LoginForm.jsx
@@ -1,23 +1,49 @@
 import { useState } from "react";
-import { Button, Input } from "@nextui-org/react";
-import { Link } from "react-router-dom";
+import { Button, Input, Spinner } from "@nextui-org/react";
+import { Link, useLocation, useNavigate } from "react-router-dom";
 import axiosInstance from "../../utils/axiosInstance";
+import toast from "react-hot-toast";
 
 const LoginForm = () => {
+  const [isLoading, setIsLoading] = useState(false);
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
+  const navigate = useNavigate();
+  const location = useLocation();
+  const from = location.state?.from?.pathname || "/";
+
   const handleFormSubmit = (event) => {
     event.preventDefault();
+    setIsLoading(true);
 
     axiosInstance
       .post("/api/users/login", {
         email,
         password,
       })
-      .then((res) => {
-        console.log(res.data);
-        alert(res.data.message);
+      .then((response) => {
+        const token = response.data?.token;
+        axiosInstance
+          .get(`/api/users/profile?token=${token}`)
+          .then((res) => {
+            setIsLoading(false);
+            // save token and profile on local storage
+            localStorage.setItem("token", JSON.stringify(token));
+            localStorage.setItem("profile", JSON.stringify(res.data));
+            // After login, redirect back to the page they came from
+            navigate(from, { replace: true });
+          })
+          .catch(() => {
+            toast.error("Sorry! Something went wrong");
+            setIsLoading(false);
+          });
+      })
+      .catch((error) => {
+        toast.error(
+          error?.response?.data?.message || "Sorry! Something went wrong"
+        );
+        setIsLoading(false);
       });
   };
 
@@ -39,11 +65,13 @@ const LoginForm = () => {
           onChange={(e) => setPassword(e.target.value)}
         />
         <Button
+          isDisabled={isLoading}
           type="submit"
           radius="full"
           className="mt-4 bg-gradient-to-r from-[#39393F] via-[#47474e] to-[#39393F] text-white shadow-lg"
         >
-          Login
+          {!isLoading && <span>Login</span>}
+          {isLoading && <Spinner />}
         </Button>
         <h5 className="text-[13px] text-center mt-4 flex gap-2 justify-center">
           <span>Not registered yet?</span>
