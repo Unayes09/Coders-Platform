0121c82984a69fe88ef66a043cc0c5b13fd39add
Mutah3r <evil.programmer.stephen@gmail.com>
2024-08-15 18:19:09 +0600

    add sidebar to the chatbot

--- frontend/src/pages/Chatbot/ChatSidebar.jsx
+++ frontend/src/pages/Chatbot/ChatSidebar.jsx
@@ -1,7 +1,113 @@
-const ChatSidebar = () => {
+import { Button, Tooltip } from "@nextui-org/react";
+import Logo from "../../components/Logo/Logo";
+import { CiEdit } from "react-icons/ci";
+import { SlOptions } from "react-icons/sl";
+import { TbLayoutSidebarLeftCollapse } from "react-icons/tb";
+
+const ChatSidebar = ({ isSidebarHidden, setIsSidebarHidden }) => {
   return (
-    <div>
-      <h1>This is chat sidebar</h1>
+    <div className={`flex flex-col gap-4`}>
+      <div className="flex flex-col gap-2">
+        <Tooltip placement="right" content="Hide sidebar" color="secondary">
+          <Button
+            className="self-start"
+            onClick={() => setIsSidebarHidden(!isSidebarHidden)}
+            size="sm"
+          >
+            <TbLayoutSidebarLeftCollapse className="text-xl" />
+          </Button>
+        </Tooltip>
+        <Button className="bg-[#282c34] text-white shadow-lg w-full flex gap-2 items-center justify-between group">
+          <div className="flex items-center justify-start gap-2">
+            <Logo />
+            <span>New Chat</span>
+          </div>
+          <CiEdit className="hidden group-hover:block font-bold text-[18px]" />
+        </Button>
+      </div>
+
+      <div className="overflow-y-auto chatbot-sidebar-list-holder">
+        <ul className="flex flex-col gap-2">
+          <li className="py-2 px-4 bg-[#181a20] hover:bg-[#282c34] rounded-lg cursor-pointer whitespace-nowrap text-ellipsis overflow-hidden relative group">
+            CV Summary And Review For Application
+            <div className="hidden rounded-md absolute top-1/2 -translate-y-1/2 right-[10px] h-[20px] w-[30px] bg-gray-700 group-hover:flex justify-center items-center">
+              <SlOptions />
+            </div>
+          </li>
+          <li className="py-2 px-4 bg-[#181a20] hover:bg-[#282c34] rounded-lg cursor-pointer whitespace-nowrap text-ellipsis overflow-hidden relative group">
+            CV Summary And Review For Application
+            <div className="hidden rounded-md absolute top-1/2 -translate-y-1/2 right-[10px] h-[20px] w-[30px] bg-gray-700 group-hover:flex justify-center items-center">
+              <SlOptions />
+            </div>
+          </li>
+          <li className="py-2 px-4 bg-[#181a20] hover:bg-[#282c34] rounded-lg cursor-pointer whitespace-nowrap text-ellipsis overflow-hidden relative group">
+            CV Summary And Review For Application
+            <div className="hidden rounded-md absolute top-1/2 -translate-y-1/2 right-[10px] h-[20px] w-[30px] bg-gray-700 group-hover:flex justify-center items-center">
+              <SlOptions />
+            </div>
+          </li>
+          <li className="py-2 px-4 bg-[#181a20] hover:bg-[#282c34] rounded-lg cursor-pointer whitespace-nowrap text-ellipsis overflow-hidden relative group">
+            CV Summary And Review For Application
+            <div className="hidden rounded-md absolute top-1/2 -translate-y-1/2 right-[10px] h-[20px] w-[30px] bg-gray-700 group-hover:flex justify-center items-center">
+              <SlOptions />
+            </div>
+          </li>
+          <li className="py-2 px-4 bg-[#181a20] hover:bg-[#282c34] rounded-lg cursor-pointer whitespace-nowrap text-ellipsis overflow-hidden relative group">
+            CV Summary And Review For Application
+            <div className="hidden rounded-md absolute top-1/2 -translate-y-1/2 right-[10px] h-[20px] w-[30px] bg-gray-700 group-hover:flex justify-center items-center">
+              <SlOptions />
+            </div>
+          </li>
+          <li className="py-2 px-4 bg-[#181a20] hover:bg-[#282c34] rounded-lg cursor-pointer whitespace-nowrap text-ellipsis overflow-hidden relative group">
+            CV Summary And Review For Application
+            <div className="hidden rounded-md absolute top-1/2 -translate-y-1/2 right-[10px] h-[20px] w-[30px] bg-gray-700 group-hover:flex justify-center items-center">
+              <SlOptions />
+            </div>
+          </li>
+          <li className="py-2 px-4 bg-[#181a20] hover:bg-[#282c34] rounded-lg cursor-pointer whitespace-nowrap text-ellipsis overflow-hidden relative group">
+            CV Summary And Review For Application
+            <div className="hidden rounded-md absolute top-1/2 -translate-y-1/2 right-[10px] h-[20px] w-[30px] bg-gray-700 group-hover:flex justify-center items-center">
+              <SlOptions />
+            </div>
+          </li>
+          <li className="py-2 px-4 bg-[#181a20] hover:bg-[#282c34] rounded-lg cursor-pointer whitespace-nowrap text-ellipsis overflow-hidden relative group">
+            CV Summary And Review For Application
+            <div className="hidden rounded-md absolute top-1/2 -translate-y-1/2 right-[10px] h-[20px] w-[30px] bg-gray-700 group-hover:flex justify-center items-center">
+              <SlOptions />
+            </div>
+          </li>
+          <li className="py-2 px-4 bg-[#181a20] hover:bg-[#282c34] rounded-lg cursor-pointer whitespace-nowrap text-ellipsis overflow-hidden relative group">
+            CV Summary And Review For Application
+            <div className="hidden rounded-md absolute top-1/2 -translate-y-1/2 right-[10px] h-[20px] w-[30px] bg-gray-700 group-hover:flex justify-center items-center">
+              <SlOptions />
+            </div>
+          </li>
+          <li className="py-2 px-4 bg-[#181a20] hover:bg-[#282c34] rounded-lg cursor-pointer whitespace-nowrap text-ellipsis overflow-hidden relative group">
+            CV Summary And Review For Application
+            <div className="hidden rounded-md absolute top-1/2 -translate-y-1/2 right-[10px] h-[20px] w-[30px] bg-gray-700 group-hover:flex justify-center items-center">
+              <SlOptions />
+            </div>
+          </li>
+          <li className="py-2 px-4 bg-[#181a20] hover:bg-[#282c34] rounded-lg cursor-pointer whitespace-nowrap text-ellipsis overflow-hidden relative group">
+            CV Summary And Review For Application
+            <div className="hidden rounded-md absolute top-1/2 -translate-y-1/2 right-[10px] h-[20px] w-[30px] bg-gray-700 group-hover:flex justify-center items-center">
+              <SlOptions />
+            </div>
+          </li>
+          <li className="py-2 px-4 bg-[#181a20] hover:bg-[#282c34] rounded-lg cursor-pointer whitespace-nowrap text-ellipsis overflow-hidden relative group">
+            CV Summary And Review For Application
+            <div className="hidden rounded-md absolute top-1/2 -translate-y-1/2 right-[10px] h-[20px] w-[30px] bg-gray-700 group-hover:flex justify-center items-center">
+              <SlOptions />
+            </div>
+          </li>
+          <li className="py-2 px-4 bg-[#181a20] hover:bg-[#282c34] rounded-lg cursor-pointer whitespace-nowrap text-ellipsis overflow-hidden relative group">
+            CV Summary And Review For Application
+            <div className="hidden rounded-md absolute top-1/2 -translate-y-1/2 right-[10px] h-[20px] w-[30px] bg-gray-700 group-hover:flex justify-center items-center">
+              <SlOptions />
+            </div>
+          </li>
+        </ul>
+      </div>
     </div>
   );
 };
--- frontend/src/pages/Chatbot/Chatbot.jsx
+++ frontend/src/pages/Chatbot/Chatbot.jsx
@@ -1,10 +1,53 @@
+import { useState } from "react";
 import ChatSidebar from "./ChatSidebar";
+import { Button } from "@nextui-org/react";
+import { TbLayoutSidebarLeftExpand } from "react-icons/tb";
+import { Tooltip } from "@nextui-org/tooltip";
+import "./Chatbot.css";
 
 const Chatbot = () => {
+  const [isSidebarHidden, setIsSidebarHidden] = useState(false);
+
   return (
     <div className="mx-6 mt-2 pb-4">
-      <ChatSidebar />
-      <div>This is chat content holder</div>
+      <div className="flex flex-col md:grid grid-cols-5">
+        {!isSidebarHidden && (
+          <div className="col-span-1 px-2">
+            <ChatSidebar
+              isSidebarHidden={isSidebarHidden}
+              setIsSidebarHidden={setIsSidebarHidden}
+            />
+          </div>
+        )}
+        <div
+          className={`${
+            isSidebarHidden
+              ? "col-span-5 flex gap-4 items-center"
+              : "col-span-4"
+          } mx-2`}
+        >
+          {isSidebarHidden && (
+            <div>
+              <Tooltip
+                placement="bottom"
+                content="Show sidebar"
+                color="secondary"
+              >
+                <Button
+                  className="self-start"
+                  onClick={() => setIsSidebarHidden(!isSidebarHidden)}
+                  size="sm"
+                >
+                  <TbLayoutSidebarLeftExpand className="text-xl" />
+                </Button>
+              </Tooltip>
+            </div>
+          )}
+          <span className="text-xl font-bold bg-clip-text text-transparent bg-gradient-to-r from-pink-500 to-purple-500">
+            Chatbot
+          </span>
+        </div>
+      </div>
     </div>
   );
 };
