4acefbfd07204fb94aa3094374627e279570d17b
Mutah3r <evil.programmer.stephen@gmail.com>
2024-08-21 13:17:05 +0600

    get chatbot chat for a user

--- frontend/src/pages/Chatbot/ChatSidebar.jsx
+++ frontend/src/pages/Chatbot/ChatSidebar.jsx
@@ -12,16 +12,35 @@ import { SlOptions } from "react-icons/sl";
 import { TbLayoutSidebarLeftCollapse } from "react-icons/tb";
 import { EditDocumentIcon } from "./EditDocumentIcon";
 import { DeleteDocumentIcon } from "./DeleteDocumentIcon";
+import { useContext, useEffect, useState } from "react";
+import axiosInstance from "../../utils/axiosInstance";
+import { UserContext } from "../../providers/UserProvider";
 
 const ChatSidebar = ({ isSidebarHidden, setIsSidebarHidden }) => {
+  const [chatList, setChatList] = useState([]);
+  const [refetch, setRefetch] = useState(false);
+
+  const { user } = useContext(UserContext);
+
+  useEffect(() => {
+    axiosInstance
+      .get(`/api/bot/chats?ownerEmail=${user?.email}&token=${user?.token}`)
+      .then((res) => {
+        setChatList(res.data);
+        console.log(res.data);
+      });
+  }, [refetch, user]);
+
   const iconClasses =
     "text-xl text-default-500 pointer-events-none flex-shrink-0";
 
-  const handleRenameChat = () => {
+  const handleRenameChat = (chatId) => {
+    console.log(chatId);
     alert("rename");
   };
 
-  const handleDeleteChat = () => {
+  const handleDeleteChat = (chatId) => {
+    console.log(chatId);
     alert("delete chat");
   };
 
@@ -48,46 +67,55 @@ const ChatSidebar = ({ isSidebarHidden, setIsSidebarHidden }) => {
 
       <div className="overflow-y-auto chatbot-sidebar-list-holder">
         <ul className="flex flex-col gap-2">
-          <li className="py-2 px-4 bg-[#181a20] hover:bg-[#282c34] rounded-lg cursor-pointer whitespace-nowrap text-ellipsis overflow-hidden relative group">
-            CV Summary And Review For Application
-            <Dropdown className="bg-[#27272A] text-white hover:text-white">
-              <DropdownTrigger>
-                <div className="hidden rounded-md absolute top-1/2 -translate-y-1/2 right-[10px] h-[20px] w-[30px] bg-gray-700 group-hover:flex justify-center items-center">
-                  <SlOptions />
-                </div>
-              </DropdownTrigger>
-              <DropdownMenu
-                variant="flat"
-                aria-label="Dropdown menu with description"
+          {chatList.map((chat) => {
+            return (
+              <li
+                key={chat.id}
+                className="py-2 px-4 bg-[#181a20] hover:bg-[#282c34] rounded-lg cursor-pointer whitespace-nowrap text-ellipsis overflow-hidden relative group"
               >
-                <DropdownItem
-                  color="primary"
-                  key="edit"
-                  showDivider
-                  description="Allows you to edit the file"
-                  startContent={<EditDocumentIcon className={iconClasses} />}
-                  onClick={handleRenameChat}
-                >
-                  Edit file
-                </DropdownItem>
+                {chat.chatName}
+                <Dropdown className="bg-[#27272A] text-white hover:text-white">
+                  <DropdownTrigger>
+                    <div className="hidden rounded-md absolute top-1/2 -translate-y-1/2 right-[10px] h-[20px] w-[30px] bg-gray-700 group-hover:flex justify-center items-center">
+                      <SlOptions />
+                    </div>
+                  </DropdownTrigger>
+                  <DropdownMenu
+                    variant="flat"
+                    aria-label="Dropdown menu with description"
+                  >
+                    <DropdownItem
+                      color="primary"
+                      key="edit"
+                      showDivider
+                      description="Allows you to edit the file"
+                      startContent={
+                        <EditDocumentIcon className={iconClasses} />
+                      }
+                      onClick={() => handleRenameChat(chat.id)}
+                    >
+                      Rename
+                    </DropdownItem>
 
-                <DropdownItem
-                  key="delete"
-                  className="text-danger"
-                  color="danger"
-                  description="Permanently delete the file"
-                  startContent={
-                    <DeleteDocumentIcon
-                      className={iconClasses + "text-danger"}
-                    />
-                  }
-                  onClick={handleDeleteChat}
-                >
-                  Delete file
-                </DropdownItem>
-              </DropdownMenu>
-            </Dropdown>
-          </li>
+                    <DropdownItem
+                      key="delete"
+                      className="text-danger"
+                      color="danger"
+                      description="Permanently delete the file"
+                      startContent={
+                        <DeleteDocumentIcon
+                          className={iconClasses + "text-danger"}
+                        />
+                      }
+                      onClick={() => handleDeleteChat(chat.id)}
+                    >
+                      Delete
+                    </DropdownItem>
+                  </DropdownMenu>
+                </Dropdown>
+              </li>
+            );
+          })}
         </ul>
       </div>
     </div>
