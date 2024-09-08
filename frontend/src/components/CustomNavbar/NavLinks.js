64b2a3f4a0c39b29851b1b3fbf2d641bfc796e50
Unayes09 <unayeskhan.0808@gmail.com>
2024-09-08 16:40:31 +0600

    news portal

--- frontend/src/components/CustomNavbar/NavLinks.js
+++ frontend/src/components/CustomNavbar/NavLinks.js
@@ -1,9 +1,11 @@
 const navLinks = [
   { text: "Playground", to: "/playground" },
+  { text: "News", to: "/news" },
   { text: "Depots", to: "/depots" },
   { text: "Q&A", to: "/qna" },
   { text: "Skill Test", to: "/skill-test" },
   { text: "Chatbot", to: "/chatbot" },
+  
 ];
 
 export default navLinks;
