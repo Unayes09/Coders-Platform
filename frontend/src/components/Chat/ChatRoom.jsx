0e26bb81efc0c9ff3de670d3df2f7abe235bb294
Mutah3r <evil.programmer.stephen@gmail.com>
2024-09-17 19:05:30 +0600

    modify chat settings

--- frontend/src/components/Chat/ChatRoom.jsx
+++ frontend/src/components/Chat/ChatRoom.jsx
@@ -5,7 +5,7 @@ const ChatPage = () => {
     <div className="mx-6 mt-2 pt-2 pb-6 h-[90vh]">
       <PrettyChatWindow
         projectId="0e16047b-9510-4874-89dd-1cbce8ada1d7"
-        username="john_smith"
+        username="Unayes"
         secret="5d633895-77af-4e14-8888-e3611aba8cea"
         // style={{ height: "100vh" }}
       />
