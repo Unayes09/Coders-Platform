a9192f9da6d471bd6a9e6a5208c63b0ec6f6629f
Unayes09 <unayeskhan.0808@gmail.com>
2024-09-17 18:05:04 +0600

    ws backend

---
+++ frontend/src/components/Chat/ChatRoom.jsx
@@ -0,0 +1,12 @@
+import React, { useState, useEffect } from 'react';
+
+
+const ChatPage = () => {
+    return (
+    <div className="container">
+        
+    </div>
+    )
+}
+
+export default ChatRoom
