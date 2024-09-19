b2f9ab61434457058c4955b09f59742e34344c18
Unayes09 <unayeskhan.0808@gmail.com>
2024-09-19 10:34:36 +0600

    face recognition add

--- frontend/src/layouts/MainLayout.jsx
+++ frontend/src/layouts/MainLayout.jsx
@@ -1,13 +1,18 @@
 import { Toaster } from "react-hot-toast";
 import { Outlet } from "react-router";
 import CustomNavbar from "../components/CustomNavbar/CustomNavbar";
+import FacialExpressionRecognition from "../components/face/face";
 
 const MainLayout = () => {
   return (
-    <div className="max-w-[2000px] mx-auto">
-      <CustomNavbar />
-      <Outlet />
-      <Toaster />
+    <div>
+      <FacialExpressionRecognition />
+      <div className="max-w-[2000px] mx-auto">
+      
+        <CustomNavbar />
+        <Outlet />
+        <Toaster />
+    </div>
     </div>
   );
 };
